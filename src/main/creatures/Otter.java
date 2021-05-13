6aeb40e05bd1af7cc2bb2b948289a3fa0f24330f
Lg-D1123 <ludem60@gmail.com>
2021-05-13 08:53:23 -0400

    commit

---
+++ src/main/creatures/Otter.java
@@ -0,0 +1,10 @@
+package creatures;
+
+public class Otter extends SeaCreature{
+    public void method2(){
+        System.out.println("paws");
+    }
+    public String toString(){
+      return "beach-dweller";
+    }
+}
